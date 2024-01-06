cd505c1ce7d02ecbfe0a5288f100ee136f006d11
gustavo giao <gustavogiao@ua.pt>
2024-01-06 18:09:12 +0000

    Alterações II

---
+++ CalculadoraDeJuroComposto.js
@@ -0,0 +1,6 @@
+
+    function scrollToCalculadoras() {
+        var calculadorasSection = document.getElementById('calculadoras');
+        calculadorasSection.scrollIntoView({ behavior: 'smooth' });
+    }
+
