3c188164578c1ccfcda023727043945446ce3d4a
gustavo giao <gustavogiao@ua.pt>
2024-01-12 16:36:13 +0000

    Alterações Gerais

--- CalculadoraDeJuroComposto.js
+++ CalculadoraDeJuroComposto.js
@@ -4,3 +4,16 @@
         calculadorasSection.scrollIntoView({ behavior: 'smooth' });
     }
 
+    document.addEventListener('DOMContentLoaded', function () {
+        var urlParams = new URLSearchParams(window.location.search);
+        var scrollToCalculadoras = urlParams.get('scrollToCalculadoras');
+
+        if (scrollToCalculadoras === 'true') {
+            var calculadorasSection = document.getElementById('calculadoras');
+            
+            if (calculadorasSection) {
+                calculadorasSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    });
+   
